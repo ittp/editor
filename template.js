855e3b35b508764c0a553dcebb19a80a8594cca0
ittp <61617660+ittp@users.noreply.github.com>
2021-09-01 23:11:06 +0300

    Create template.js

---
+++ template.js
@@ -0,0 +1,22 @@
+var blocks = []
+
+var template = {
+time: 1556098174501,
+blocks: [
+{
+type: "header",
+data: {
+  text: "Editor.js",
+  level: 2,
+  justify: "center"
+}
+},
+{
+type: "table",
+content: {}
+}
+],
+version: "2.12.4"
+}
+
+module.exports = template
